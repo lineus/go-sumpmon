fc3228a92640887700367357a8882c9832b0896e
Kevin Huff <lineus@gmail.com>
2021-02-05 13:40:08 -0500

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lineus/go-sumpmon
+
+go 1.15
---
+++ sumpmon.go
@@ -0,0 +1,49 @@
+package sumpmon
+
+import (
+	"database/sql"
+	"log"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3" // use sqlite3
+)
+
+// Logger - a thing
+type Logger struct {
+	db *sql.DB
+}
+
+// SaveLog - saves a log
+func (logger Logger) SaveLog(action string, result string) (sql.Result, error) {
+	var epoch = time.Now().Unix()
+	stmt, err := logger.db.Prepare("INSERT INTO logs(epoch, action, result) values(?,?,?);")
+	if err != nil {
+		log.Fatal("PREPARE FAILED: ", err)
+	}
+	res, err := stmt.Exec(epoch, action, result)
+	if err != nil {
+		log.Fatal("INSERT FAILED: ", err)
+	}
+
+	return res, err
+}
+
+// Init - connect to the db and get your Logger instance
+func Init(dsn string) (Logger, error) {
+	var l Logger
+	db, err := sql.Open("sqlite3", dsn)
+	l.db = db
+
+	_, err = l.db.Exec(`CREATE TABLE IF NOT EXISTS logs(
+		id INTEGER PRIMARY KEY ASC,
+		epoch INTEGER,
+		action TEXT,
+		result TEXT
+	)`)
+
+	if err != nil {
+		log.Fatal("CREATE TABLE FAILED: ", err)
+	}
+
+	return l, err
+}
